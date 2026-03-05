f9db299d6ab02ac4d6b5158033665a6e35d982ab
Vinicios Rosa <viniciosrp.dev@hotmail.com>
2026-03-05 18:32:33 -0300

    Project Library started

---
+++ src/test/java/com/vinicios/library/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicios.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
