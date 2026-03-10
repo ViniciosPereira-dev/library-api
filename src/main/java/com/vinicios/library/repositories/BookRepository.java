932e99daaef05c7522199abcf77356430da844c8
Vinicios Rosa <viniciosrp.dev@hotmail.com>
2026-03-10 13:33:39 -0300

    feat: add JPA repositories

--- src/main/java/com/vinicios/library/repositories/BookRepository.java
+++ src/main/java/com/vinicios/library/repositories/BookRepository.java
@@ -4,6 +4,16 @@ import com.vinicios.library.entities.Book;
 import com.vinicios.library.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    List<Book> findByAvailableTrue();
+
+    List<Book> findByAuthorIgnoreCase(String author);
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    Optional<Book> findByIsbn(String isbn);
 }
